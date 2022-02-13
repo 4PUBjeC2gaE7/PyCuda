788f5328ef2511d32a32c8d0efc74263312a4aaa
Hector Malagon <08110ector.0x4Dalagon@gmail.com>
2022-02-12 21:44:57 -0700

    Updated to use f-strings; Added device attributes;

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import pycuda.driver as drv
 
 drv.init()
 
-print ('Detected {} CUDA capable devices'.format(drv.Device.count()))
+print (f'Detected {drv.Device.count()} CUDA capable devices\n')
 
 for i in range (drv.Device.count()):
     gpu_device = drv.Device(i)
@@ -13,3 +13,7 @@ for i in range (drv.Device.count()):
     compute_capability = float( '%d.%d' % gpu_device.compute_capability())
     print(f'\tComputer Capability: {compute_capability}')
     print(f'\tTotal Memory: {gpu_device.total_memory() // 1024**2} MB')
+    device_attrs = gpu_device.get_attributes()
+    for key, attr in device_attrs.items():
+        print(f'{str(key):>40}: {attr}')
+    num_mp = gpu_device.get_attribute(drv.device_attribute.MULTIPROCESSOR_COUNT)
