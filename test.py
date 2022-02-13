cdb5df08ad3ef8937676e5a19ddf1c90e2b79b06
Hector Malagon <08110ector.0x4Dalagon@gmail.com>
2022-02-12 19:23:38 -0700

    added numpy and pycuda started capability intfc

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print('hello world')
+import imp
+
+
+import pycuda.driver as drv
+
+drv.init()
+
+print ('Detected {} CUDA capable devices'.format(drv.Device.count()))
+
+for i in range (drv.Device.count()):
+    gpu_device = drv.Device(i)
+    print(f'Device {i}: {gpu_device.name()}')
+    compute_capability = float( '%d.%d' % gpu_device.compute_capability())
+    print(f'\tComputer Capability: {compute_capability}')
+    print(f'\tTotal Memory: {gpu_device.total_memory() // 1024**2} MB')
