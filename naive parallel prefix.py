f72582501f099ae4b23002afe64035d04e159300
Hector Malagon <08110ector.0x4Dalagon@gmail.com>
2022-03-21 20:43:04 -0600

    Added timing calculations

--- naive parallel prefix.py
+++ naive parallel prefix.py
@@ -36,11 +36,15 @@ if __name__ == '__main__':
     testVec_gpu = gpuarray.to_gpu(testVec)
 
     outVec_gpu = gpuarray.empty_like(testVec_gpu)
+    t1 = time()
     naive_gpu(testVec_gpu, outVec_gpu, block=(1024,1,1), grid=(1,1,1))
-
+    t2 = time()
     totalSum = sum(testVec)
+    t3 = time()
     totalSum_gpu = outVec_gpu[-1].get()
 
     isMatch = 'true' if np.allclose(totalSum, totalSum_gpu) else 'false'
     print(f'Data Match: {isMatch}')
 
+    print(f'GPU time: {(t2 - t1)*1000:2.4f}ms')
+    print(f'CPU time: {(t3 - t2)*1000:2.4f}ms')
