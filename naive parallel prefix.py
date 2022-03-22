77867714158ec536389d05f751d5616f74d69706
Hector Malagon <08110ector.0x4Dalagon@gmail.com>
2022-03-21 20:40:39 -0600

    adding parallel prefix to vcs

---
+++ naive parallel prefix.py
@@ -0,0 +1,46 @@
+import pycuda.autoinit
+import pycuda.driver as drv
+from pycuda import gpuarray
+from pycuda.compiler import SourceModule
+import numpy as np
+import matplotlib.pyplot as plt
+from time import time
+
+naive_ker = SourceModule("""
+__global__ void naive_prefix(double *vec, double *out)
+{
+    __shared__ double sum_buf[1024];
+    int tid = threadIdx.x;
+    sum_buf[tid] = vec[tid];
+
+    int iter = 1;
+    for (int i=0; i<10; i++)
+    {
+        __syncthreads();
+        if (tid >= iter)
+        {
+            sum_buf[tid] = sum_buf[tid] + sum_buf[tid - iter];
+        }
+        iter = iter << 1;
+    }
+    __syncthreads();
+    out[tid] = sum_buf[tid];
+    __syncthreads();
+}
+""")
+
+naive_gpu = naive_ker.get_function("naive_prefix")
+
+if __name__ == '__main__':
+    testVec = np.random.randn(1024).astype(np.float64)
+    testVec_gpu = gpuarray.to_gpu(testVec)
+
+    outVec_gpu = gpuarray.empty_like(testVec_gpu)
+    naive_gpu(testVec_gpu, outVec_gpu, block=(1024,1,1), grid=(1,1,1))
+
+    totalSum = sum(testVec)
+    totalSum_gpu = outVec_gpu[-1].get()
+
+    isMatch = 'true' if np.allclose(totalSum, totalSum_gpu) else 'false'
+    print(f'Data Match: {isMatch}')
+
