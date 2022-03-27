f7eafa7599cbaa550a4b30fa23dc486e465ec158
Hector Malagon <08110ector.0x4Dalagon@gmail.com>
2022-03-27 13:47:18 -0600

    Messing around with plot layout

--- LifeOf_streams.py
+++ LifeOf_streams.py
@@ -70,22 +70,26 @@ def updateGpu(frameNum, images, new_gpu, old_gpu, size, streams):
 if __name__ == '__main__':
     N = 256     # frame size, in cells
     P = 0.8     # probability of cell starting alive
-    S = 4       # number of concurrent streams
+    Sx = 8      # number of 'x' frames
+    Sy = 3      # number of 'y' frames
 
     streams = []
     lattices_gpu = []
     newLattices_gpu = []
     imgs = []
 
-    fig, ax = plt.subplots(nrows = 1, ncols=S)
+    fig, ax = plt.subplots(nrows = Sy, ncols=Sx,figsize=(3*Sx,3*Sy))
+    plt.tight_layout(h_pad=None, w_pad=None)
 
-    for k in range(S):
-        streams.append(drv.Stream())
-        lattice = np.int32( np.random.choice([0,1], N*N, p=[P,1-P]).reshape(N,N) )
-        lattices_gpu.append(gpuarray.to_gpu(lattice))
-        newLattices_gpu.append(gpuarray.empty_like(lattices_gpu[k]))
-        imgs.append( ax[k].imshow( lattices_gpu[k].get_async(stream=streams[k]),
-                                   interpolation='nearest'))
+    for j in range(Sx):
+        for k in range(Sy):
+            streams.append(drv.Stream())
+            lattice = np.int32( np.random.choice([0,1], N*N, p=[P,1-P]).reshape(N,N) )
+            lattices_gpu.append(gpuarray.to_gpu(lattice))
+            newLattices_gpu.append(gpuarray.empty_like(lattices_gpu[k]))
+            imgs.append( ax[k,j].imshow( lattices_gpu[k].get_async(stream=streams[k]),
+                                       interpolation='nearest'))
+            ax[k,j].axis('off')
     
     ani = anim.FuncAnimation(fig, updateGpu, interval=0, frames=1000, save_count=1000,
         fargs=(imgs, newLattices_gpu, lattices_gpu, N, streams),
